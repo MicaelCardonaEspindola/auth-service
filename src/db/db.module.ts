fc5dadee424d620d2a2b628a031d3e52d4cfd505
CardonaEspindolaMicael <cardona.micael@ficct.uagrm.edu.bo>
2025-11-10 19:05:49 -0400

    more docker changes 2

--- src/db/db.module.ts
+++ src/db/db.module.ts
@@ -8,12 +8,13 @@ import { Logger } from '../logger/logger';
 import { LoggerModule } from '../logger/logger.module';
 import { DbConfigError } from './db.errors';
 import { DbConfig } from './db.interface';
+
 @Module({})
 export class DbModule {
   private static getConnectionOptions(config: ConfigService, dbconfig: DbConfig): TypeOrmModuleOptions {
     const dbdata = config.get().db;
     if (!dbdata) {
-      throw new DbConfigError('Database config is mSissing');
+      throw new DbConfigError('Database config is missing');
     }
     const connectionOptions = DbModule.getConnectionOptionsPostgres(dbdata);
     return {
@@ -24,19 +25,20 @@ export class DbModule {
     };
   }
 
-private static getConnectionOptionsPostgres(dbdata: ConfigDBData): TypeOrmModuleOptions {
-  return {
-    type: 'postgres',
-    url: dbdata.url,
-    ssl: true, // habilita SSL en la conexión
-    extra: {
-      ssl: {
-        rejectUnauthorized: false, // ignora el certificado autofirmado
-      },
-    },
-  };
-}
-
+  private static getConnectionOptionsPostgres(dbdata: ConfigDBData): TypeOrmModuleOptions {
+    const isProduction = process.env.NODE_ENV === 'production';
+    
+    return {
+      type: 'postgres',
+      url: dbdata.url,
+      // Usar un objeto SSL en lugar de booleano
+      ssl: isProduction ? { rejectUnauthorized: false } : false,
+      // Extra NO es necesario cuando ssl ya está configurado arriba
+      extra: isProduction ? {
+        // Configuraciones adicionales si son necesarias
+      } : {},
+    };
+  }
 
   public static forRoot(dbconfig: DbConfig): DynamicModule {
     return {
@@ -44,9 +46,9 @@ private static getConnectionOptionsPostgres(dbdata: ConfigDBData): TypeOrmModule
       imports: [
         TypeOrmModule.forRootAsync({
           imports: [ConfigModule, LoggerModule],
-          // eslint-disable-next-line @typescript-eslint/no-unused-vars
-          useFactory: (configService: ConfigService, logger: Logger) => DbModule.getConnectionOptions(configService, dbconfig),
-          inject: [ConfigService],
+          useFactory: (configService: ConfigService, logger: Logger) => 
+            DbModule.getConnectionOptions(configService, dbconfig),
+          inject: [ConfigService, Logger],
         }),
       ],
       controllers: [],
